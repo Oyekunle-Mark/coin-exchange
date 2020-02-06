44f65bfd31a1562d45d18906fafe3a06d756bfc3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-06 14:55:27 +0100

    The type definition

---
+++ index.js
@@ -0,0 +1,15 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type ExchangeResponse {
+    value: Float!
+  }
+
+  type Query {
+    exchange(
+      type: String!
+      exchangeRate: Float!
+      margin: Float!
+    ): ExchangeResponse!
+  }
+`;
