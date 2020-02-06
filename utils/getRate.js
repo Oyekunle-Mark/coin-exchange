58bb460ab3fb6f711e3dc97033198c7b94649256
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-06 16:36:58 +0100

    Returns only the rate as float

--- utils/getRate.js
+++ utils/getRate.js
@@ -3,11 +3,11 @@ const axios = require('axios');
 const URL = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 
 const api = async () => {
-//   let response = {};
+  //   let response = {};
 
   const response = (await axios.get(URL)).data;
 
   return response;
 };
 
-module.exports = api;
+module.exports = api.bpi.USD.rate_float;
