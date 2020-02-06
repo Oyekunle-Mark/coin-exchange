c2dcdeaf9e7fbbcfb20764e0e2f10ddfcd80fb7b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-06 18:26:45 +0100

    Logic for finding the convertion rate

--- utils/convert.js
+++ utils/convert.js
@@ -1,3 +1,7 @@
-const convert = (type, rate, margin) => {};
+const convert = (type, rate, margin) => {
+  const difference = rate * (margin / 100);
+
+  return type === 'sell' ? rate - difference : rate + difference;
+};
 
 module.exports = convert;
