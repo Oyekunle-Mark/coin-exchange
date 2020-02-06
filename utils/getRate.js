9ea5a5acb7e22205ee721f2bb25d269bf6410e38
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-06 16:57:43 +0100

    Wrapped in a try catch

--- utils/getRate.js
+++ utils/getRate.js
@@ -3,7 +3,11 @@ const axios = require('axios');
 const URL = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 
 module.exports = async () => {
-  const response = (await axios.get(URL)).data;
+  try {
+    const response = (await axios.get(URL)).data;
 
-  return response.bpi.USD.rate_float;
+    return response.bpi.USD.rate_float;
+  } catch (err) {
+    console.error(`Error getting data: ${err}`);
+  }
 };
