4d41a968c19d2fb48c5346c503964ff60d9197b0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-06 18:32:41 +0100

    Added docstring

--- utils/convert.js
+++ utils/convert.js
@@ -1,3 +1,10 @@
+/**
+ * Finds the price of a sale or buy
+ * @param {string} type option, buy or sell
+ * @param {number} rate the price of 1 BTC
+ * @param {number} margin the margin between buying and selling in percent
+ * @returns {number} the calculated price
+ */
 const convert = (type, rate, margin) => {
   const difference = rate * (margin / 100);
 
