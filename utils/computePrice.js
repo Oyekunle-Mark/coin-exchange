d102efe9bd9c940be7fb55fec6dbf3f6ea87a9c0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-08 09:25:55 +0100

    Improved doc string

--- utils/computePrice.js
+++ utils/computePrice.js
@@ -1,14 +1,14 @@
 /**
- * Finds the price of a sale or buy
+ * Computes the price of a sale or buy using the margin
  * @param {string} type option, buy or sell
  * @param {number} price the price of 1 BTC
  * @param {number} margin the margin between buying and selling in percent
- * @returns {number} the calculated price
+ * @returns {number} the computed price
  */
-const convert = (type, price, margin) => {
+const compute = (type, price, margin) => {
   const computedMargin = price * (margin / 100);
 
   return type === 'sell' ? price - computedMargin : price + computedMargin;
 };
 
-module.exports = convert;
+module.exports = compute;
