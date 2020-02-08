2a042b0962888f9370b72921f64221f3dcc7ceac
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-08 11:36:53 +0100

    Test return type

--- utils/__tests__/computePrice.spec.js
+++ utils/__tests__/computePrice.spec.js
@@ -1,5 +1,9 @@
 const computePrice = require('../computePrice');
 
-describe("computePrice", () => {
-    
-})
+describe('computePrice', () => {
+  it('Returns type Number', () => {
+    const result = computePrice('sell', 9700, 0.2);
+
+    expect(typeof result).toEqual('number');
+  });
+});
