d8e89464f7c32a857638265d4fd1dff6b8f0aaf3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-08 09:22:01 +0100

    Renamed variable to price

--- utils/convert.js
+++ utils/convert.js
@@ -1,14 +1,14 @@
 /**
  * Finds the price of a sale or buy
  * @param {string} type option, buy or sell
- * @param {number} rate the price of 1 BTC
+ * @param {number} price the price of 1 BTC
  * @param {number} margin the margin between buying and selling in percent
  * @returns {number} the calculated price
  */
-const convert = (type, rate, margin) => {
-  const difference = rate * (margin / 100);
+const convert = (type, price, margin) => {
+  const difference = price * (margin / 100);
 
-  return type === 'sell' ? rate - difference : rate + difference;
+  return type === 'sell' ? price - difference : price + difference;
 };
 
 module.exports = convert;
